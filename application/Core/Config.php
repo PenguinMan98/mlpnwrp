52a196a9904b2302303d5e6493d6de0a7e1be766
Joe Broderick <admin@pengyprogramming.com>
2012-10-09 08:12:31 -0600

    Added the config for real

---
+++ application/Core/Config.php
@@ -0,0 +1,11 @@
+<?php
+class Core_Config
+{
+	public static $DB_SERVER   = 'localhost';
+	public static $DB_SERVER_2 = null;
+	public static $DB_USERNAME = 'root';
+	public static $DB_PASSWORD = 'PengyProgramming';
+	public static $DB_DATABASE = 'gt_clean';
+}
+
+//include 'ConfigLocal.php'; // Used to overwrite the value of production configuration values
