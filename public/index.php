9dc2a5ed8071eef1e4acfbcbe0ec12ae8930cb37
Joseph Broderick <PenguinMan98@usa.net>
2013-04-01 21:49:19 -0600

    updated the repo to not use server include pathing. This was making it impossible to set up a testing environment.

--- public/index.php
+++ public/index.php
@@ -2,13 +2,15 @@
 
 session_start();
 
+define('INCLUDE_PATH' , 'd:/wamp/www/mlpnwrp/application/Core/');
+
 // remove the trailing slash on the URI
 if($_SERVER['REQUEST_URI'][strlen($_SERVER['REQUEST_URI'])-1] == '/'){
 	$_SERVER['REQUEST_URI'] = substr($_SERVER['REQUEST_URI'],0,strlen($_SERVER['REQUEST_URI'])-1);
 }
- 
-require_once 'Bootstrap.php'; // load everything
+
+require_once INCLUDE_PATH . 'Bootstrap.php'; // load everything
 $_bootstrap = Bootstrap::getInstance();
 
-require_once 'router.php';
+require_once INCLUDE_PATH . 'router.php';
 ?>
