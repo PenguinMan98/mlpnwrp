136fca6052846333b8d25ca89cdcef297bdaf3b8
Joseph Broderick <PenguinMan98@usa.net>
2013-04-10 19:14:35 -0600

    Made a change to only show active chat rooms

--- application/Library/Model/Data/ChatRoomProvider.php
+++ application/Library/Model/Data/ChatRoomProvider.php
@@ -3,7 +3,7 @@ class Model_Data_ChatRoomProvider extends Model_Data_ChatRoomProviderBase
 {
 	
 	public function getChatList(){
-		$strSql = 'SELECT * FROM chat_room';
+		$strSql = 'SELECT * FROM `chat_room` cr WHERE cr.`is_active` = 1';
 		$params = array();
 		return Model_Data_ChatRoomProvider::getArrayFromQuery($strSql, $params);
 	}
