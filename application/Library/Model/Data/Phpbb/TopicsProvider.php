292ef3e5dad03c05653e2adb2549b7350e0ca3ae
Joseph Broderick <PenguinMan98@usa.net>
2013-04-11 21:59:41 -0600

    Made the hot Forum Threads work

--- application/Library/Model/Data/Phpbb/TopicsProvider.php
+++ application/Library/Model/Data/Phpbb/TopicsProvider.php
@@ -1,4 +1,13 @@
 <?php
 class Model_Data_Phpbb_TopicsProvider extends Model_Data_Phpbb_TopicsProviderBase
 {
+	public function getHotTopics($count){
+		$strSql = "
+SELECT *
+FROM `phpbb_topics`
+ORDER BY `topic_last_post_time` DESC
+LIMIT " . intval($count) . " ";
+		$params = array();
+		return parent::getArrayFromQuery($strSql, $params);
+	}
 }
